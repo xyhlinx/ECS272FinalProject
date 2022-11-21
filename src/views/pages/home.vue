3bd2e30741487326d25fabc486646e8e632910b5
andychiu923 <andychiu923@gmail.com>
2022-11-21 10:16:49 -0800

    add new page for different views

--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <h1>Welcome Home</h1>
-        <BarChart v-if="dataExists" :myBarchartData="myBarData" />
+    <div id="home">
+        <div id="col1">
+            <div id="bar_view">
+                <h1>Welcome Home</h1>
+                <BarChart v-if="dataExists" :myBarchartData="myBarData" />
+            </div>
+        </div>
+        <div id="col2"></div>
     </div>
+    
 </template>
 
 <script>
@@ -41,3 +47,28 @@ export default {
 }
 
 </script>
+
+<style>
+#home {
+    display: flex;
+    height: 100%;
+    position: relative;
+    justify-content: center;
+}
+#col1 {
+    height: 100%;
+    width: 50%;
+    position: relative;
+}
+#bar_view {
+    height: 100%;
+    width: 80%;
+    position: relative;
+}
+#col2 {
+    height: 100%;
+    width: 50%;
+    position: relative;
+}
+
+</style>
