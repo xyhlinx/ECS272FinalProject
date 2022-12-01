61e97f60d9c7e0ace3d1e8e4e13db67b4ff93772
yhlin <oscarlin2008@gmail.com>
2022-12-01 00:56:57 -0800

    chg: dev: filter genres and delete unuse fields
    .gitignore: allow filtered json file
    src/assets/filter_json.py: filter out the game which doen't have any one of the genres in the
    top10 list.
    If a game has multiple genres in the top10 list, look up the tags and choose the one with the
    highest votes. If tags field doesn't exist or tags do not include top10 genres, drop that
    game.
    If a game doesn't have genres in the top10, simply drop that game.

--- src/assets/filter_json.py
+++ src/assets/filter_json.py
@@ -2,15 +2,48 @@ import json
 
 res = {}
 
-def delete_from_dict(dic, key):
+def delete_from_dict(dic, key, *args, **kwargs):
     if key in dic:
         del dic[key]
 
-def filtered_amount(dic, key, amount=1):
+def filter_by_amount(dic, key, amount=1, *args, **kwargs):
     if key in dic and isinstance(dic[key], list):
         dic[key] = [dic[key][0]] if dic[key] else []
 
-filter_dict = {'average_playtime_2weeks': delete_from_dict,
+def filter_genre(dic, key, _id, removal_ids, *args, **kwargs):
+    ordered_match = [
+        'Indie',
+        'Action',
+        'Casual',
+        'Adventure',
+        'Simulation',
+        'Strategy',
+        'RPG',
+        'Early Access',
+        'Free to Play',
+        'Sports'
+    ]
+    match = set(ordered_match)
+    if key not in dic:
+        return
+    overlap = set(dic[key]) & match
+    if len(overlap) > 1 and 'tags' in dic and isinstance(dic['tags'], dict):
+        for k, v in sorted(dic['tags'].items(), key=lambda x: -x[1]):
+            if k in overlap:
+                dic[key] = k
+                del dic['tags']
+                return
+    elif len(overlap) == 1:
+        dic[key] = key
+        if 'tags' in dic:
+            del dic['tags']
+        return
+    else:
+        pass
+    removal_ids.add(_id)
+
+filter_dict = {
+    'average_playtime_2weeks': delete_from_dict,
     'mean_playtime_2weeks': delete_from_dict,
     'median_playtime_forever': delete_from_dict,
     'detailed_description': delete_from_dict,
@@ -18,19 +51,39 @@ filter_dict = {'average_playtime_2weeks': delete_from_dict,
     'short_description': delete_from_dict,
     'support_email': delete_from_dict,
     'reviews': delete_from_dict,
-    'screenshots': filtered_amount}
+    'screenshots': filter_by_amount,
+    'genres': filter_genre,
+    'required_age': delete_from_dict,
+    'header_image': delete_from_dict,
+    'website': delete_from_dict,
+    'support_url': delete_from_dict,
+    'windows': delete_from_dict,
+    'mac': delete_from_dict,
+    'linux': delete_from_dict,
+    'metacritic_url': delete_from_dict,
+    'notes': delete_from_dict,
+    'supported_languages': delete_from_dict,
+    'full_audio_languages': delete_from_dict,
+    'packages': delete_from_dict,
+    'categories': delete_from_dict,
+    'screenshots': delete_from_dict,
+    'movies': delete_from_dict
+    }
 
 with open('data/games.json') as f:
     dset = json.load(f)
+    removal_ids = set()
     for _id, v in dset.items():
         if ('average_playtime_forever' in v
             and (v['average_playtime_forever'] == 0
             or not isinstance(v['average_playtime_forever'], int))):
-                continue
+            continue
         else:
             for key, func in filter_dict.items():
-                func(v, key)
+                func(v, key, _id, removal_ids)
             res[_id] = v
+    for rid in removal_ids:
+        del res[rid]
 
-with open('filtered.json', 'w') as wf:
+with open('./data/filtered_data.json', 'w') as wf:
     wf.write(json.dumps(res, indent=4))
