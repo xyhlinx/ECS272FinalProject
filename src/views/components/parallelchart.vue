2e37a281c4e29811c16ed7f1938e8cd821541677
andychiu923 <andychiu923@gmail.com>
2022-12-02 11:44:41 -0800

    Add brush selection for parallel chart

--- src/views/components/parallelchart.vue
+++ src/views/components/parallelchart.vue
@@ -4,13 +4,16 @@
 
 <script>
 import * as d3 from "d3";
-import rawdata from "../../assets/data/filtered.json"
+import rawdata from "../../assets/data/filtered_data.json"
 export default {
     name: 'ParallelChart',
     data() {
         return {
             process_data: undefined,
-            keys: ["price", "metacritic_score", "positive", "achievements", "average_playtime_forever"],
+            selected_data: undefined,
+            keys: ["price", "metacritic_score", "achievements", "recommendations", "average_playtime_forever"],
+            genres: ["Indie", "Action", "Casual", "Adventure","Simulation", 
+                    "Strategy", "RPG", "Early Access", "Free to Play", "Sports"],
         }
     },
     props: {
@@ -38,7 +41,7 @@ export default {
         drawParallelChart(data, id) {
             const margin = { top: 30, right: 10, bottom: 30, left: 10 };
             const height = this.keys.length * 120;
-            const width = 700;
+            const width = 800;
             const brushHeight = 50;
 
             const svg = d3.select(id).append("svg")
@@ -55,15 +58,18 @@ export default {
                 const selected = [];
                 path.each(function(d) {
                   const active = Array.from(selections).every(([key, [min, max]]) => d[key] >= min && d[key] <= max);
-                  d3.select(this).style("stroke", active ? "#69b3a2" : "#ddd");
+                  d3.select(this).style("stroke", active ? colors(d.genres) : "#ddd");
                   if (active) {
                     d3.select(this).raise();
                     selected.push(d);
                   }
                 });
                 console.log(selected)
+                this.selected_data = selected;
                 svg.property("value", selected).dispatch("input");
             }
+
+            const colors = d3.scaleOrdinal().domain(this.genres).range(d3.schemeSet3);
 
             const brush = d3.brushX()
                             .extent([
@@ -71,9 +77,24 @@ export default {
                             [width - margin.right, brushHeight / 2]
                             ])
                             .on("start brush end", brushed);
-            
+
             const x = new Map(Array.from(this.keys, key => [key, d3.scaleLinear(d3.extent(data, d => d[key]), [margin.left, width - margin.right])]))
             const y = d3.scalePoint(this.keys, [margin.top, height - margin.bottom])
+            
+            const line = d3.line().defined(([, value]) => value != null)
+                            .x(([key, value]) => x.get(key)(value))
+                            .y(([key]) => y(key))
+
+            // line
+            const path = svg.append("g")
+                .attr("fill", "none")
+                .attr("stroke-width", 1.5)
+                .attr("stroke-opacity", 0.4)
+                .selectAll("path")
+                .data(data)
+                .join("path")
+                .attr("stroke", function(d) { return (colors(d.genres)); })
+                .attr("d", d => line(d3.cross(this.keys, [d], (key, d) => [key, d[key]])))
 
             // axis
             svg.append("g")
@@ -88,30 +109,13 @@ export default {
                     .attr("text-anchor", "start")
                     .attr("fill", "currentColor")
                     .text(d => d))
-                .call(g => g.append("text")
+                .call(g => g.selectAll("text")
                     .clone(true).lower()
                     .attr("fill", "none")
                     .attr("stroke-width", 5)
                     .attr("stroke-linejoin", "round")
                     .attr("stroke", "white"))
                 .call(brush)
-            
-            const line = d3.line().defined(([, value]) => value != null)
-                            .x(([key, value]) => x.get(key)(value))
-                            .y(([key]) => y(key))
-
-            // line
-            const path = svg.append("g")
-                .attr("fill", "none")
-                .attr("stroke-width", 1.5)
-                .attr("stroke-opacity", 0.4)
-                .selectAll("path")
-                .data(data)
-                .join("path")
-                .attr("stroke", "#69b3a2")
-                .attr("d", d => line(d3.cross(this.keys, [d], (key, d) => [key, d[key]])))
-
-            
         }
     }
 }
